610b7aa3e6867a683a3407e765611ef05f00add1
Tomass Vite <46499387+TVite@users.noreply.github.com>
2019-11-19 12:11:25 +0200

    update: change RedApple.js output color
    Red text.

--- scripts/RedApple.js
+++ scripts/RedApple.js
@@ -1,2 +1,2 @@
-globalString += "The witch hands out a Red Apple. The princess took the Red Apple.<br>";
+globalString += "<font color='red'>The witch hands out a Red Apple. The princess took the Red Apple.</font><br>";
 updateRoot();
